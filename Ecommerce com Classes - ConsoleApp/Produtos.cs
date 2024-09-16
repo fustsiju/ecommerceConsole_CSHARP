e8df21c28da2875a138a64aa1e854db2d8c7bd46
Fulvio Stefanine <62194735+fustsiju@users.noreply.github.com>
2024-09-16 15:53:00 -0300

    Add files via upload

---
+++ Ecommerce com Classes - ConsoleApp/Produtos.cs
@@ -0,0 +1,22 @@
+﻿class SistemaProd
+{
+    private DatabaseManager dbManager;
+
+    public SistemaProd(DatabaseManager databaseManager)
+    {
+        dbManager = databaseManager;
+    }
+    public void registerProd(out string nomeProd,out float valProd,out int quantProd)
+    {
+        Console.WriteLine("Insira o nome do produto a dar entrada: ");
+        nomeProd = Console.ReadLine();
+        Console.WriteLine("Insira o valor do produto: ");
+        valProd = float.Parse(Console.ReadLine());
+        Console.WriteLine("Insira a quantidade: ");
+        quantProd = int.Parse(Console.ReadLine());
+
+        dbManager.addProd(nomeProd, valProd, quantProd);
+    }
+
+
+}
---
+++ Ecommerce com Classes - ConsoleApp/SistemaLogin.cs
@@ -0,0 +1,20 @@
+﻿class SistemaLogin
+{
+    private DatabaseManager dbManager;
+
+    public SistemaLogin(DatabaseManager databaseManager)
+    {
+        dbManager = databaseManager;
+    }
+
+    public bool loginUser(string username, string password)
+    {
+        string storedPasswordHash = dbManager.GetPasswordHash(username);
+        return storedPasswordHash == password;  
+    }
+
+    public void RegisterUser(string username, string password)
+    {
+        dbManager.addUser(username, password);
+    }
+}
